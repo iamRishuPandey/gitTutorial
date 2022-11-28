78e26eee389d96080881d9f91a23fb2a0579955a
iamRishuPandey <rishup664@gmail.com>
2022-11-28 11:20:18 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
- /* Data Types:
- undefined, null, boolean,string,symbol,number, and object
- */
+
 // Examin the document object
 // console.log(document.domain);
 // console.log(document.URL);
@@ -15,25 +13,35 @@
 // console.log(document.link);
 //console.log(document.images);
 //console.log(document.getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
+//var headerTitle = document.getElementById('header-title');
+//var header = document.getElementById('main-header');
 //console.log(headerTitle);
 //headerTitle.textContent = 'Hello';
 //headerTitle.innerText = 'goodBye';
 //console.log(headerTitle.innerText );
-headerTitle.innerHTML = '<h3>Hello</h3>';
-header.style.borderBottom = 'solid 3px #000';
+//headerTitle.innerHTML = '<h3>Hello</h3>';
+//header.style.borderBottom = 'solid 3px #000';
 //getElements by className
-var title = document.getElementsByClassName('title');
-console.log(title);
-title[0].style.fontWeight = 'bold';
-title[0].style.color= 'green';
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello1';
-items[2].style.backgroundColor = 'green';
+// var title = document.getElementsByClassName('title');
+// console.log(title);
+// title[0].style.fontWeight = 'bold';
+// title[0].style.color= 'green';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello1';
+// items[2].style.backgroundColor = 'green';
 
-for(var i = 0; i < items.length; i++) {
-    items[i].style.fontWeight = 'bold';
+// for(var i = 0; i < items.length; i++) {
+//     items[i].style.fontWeight = 'bold';
+// }
+//GETELEMENT BY TAGNAME
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello1';
+li[2].style.backgroundColor = 'green';
+
+for(var i = 0; i < li.length; i++) {
+    li[i].style.fontWeight = 'bold';
 }
