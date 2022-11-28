20c5ffd39495b638912bbe6e3b2cbf695cb46044
iamRishuPandey <rishup664@gmail.com>
2022-11-28 12:35:24 +0530

    updated app.js

--- app.js
+++ app.js
@@ -36,12 +36,28 @@
 //     items[i].style.fontWeight = 'bold';
 // }
 //GETELEMENT BY TAGNAME
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent = 'Hello1';
-li[2].style.backgroundColor = 'green';
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello1';
+// li[2].style.backgroundColor = 'green';
 
-for(var i = 0; i < li.length; i++) {
-    li[i].style.fontWeight = 'bold';
+// for(var i = 0; i < li.length; i++) {
+//     li[i].style.fontWeight = 'bold';
+// }
+//querySelector
+// var header = document.querySelector('#main-header');
+// header.style.borderbottom = 'solid 4px red';
+// var input = document.querySelector('input');
+// input.value = 'Hello World';
+// var item = document.querySelector('.list-group-item:nth-child(2)');
+// item.style.color = 'green';
+//querySelectorAll
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0; i < odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
 }
