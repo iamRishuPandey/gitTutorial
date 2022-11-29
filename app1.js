5bb2affa75878003fbe6befdb40211ad6fcd414f
iamRishuPandey <rishup664@gmail.com>
2022-11-29 10:59:09 +0530

    third commit

---
+++ app1.js
@@ -0,0 +1,89 @@
+
+// TRAVERSING THE DOM //
+var itemList = document.querySelector('#items');
+// parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// childNodes
+// console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// // FirstChild
+// console.log(itemList.firstChild);
+// // firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+
+// lastChild
+// console.log(itemList.lastChild);
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+// nextSibling
+// console.log(itemList.nextSibling);
+// // nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+// previousElementSibling
+// console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
+
+// createElement
+
+// Create a div
+var newDiv =  document.createElement('div');
+
+// Add class
+newDiv.className= 'hello';
+
+// Add id
+newDiv.id = 'hello1';
+
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
+
+// Create text node
+var newDivText = document.createTextNode('Hello World');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+
+container.insertBefore(newDiv, h1);
+
+var list = document.getElementById("items");
+var newDiv1 =  document.createElement('div');
+// Add class
+newDiv1.className= 'hello';
+
+// Add id
+newDiv1.id = 'hello1';
+
+// Add attr
+newDiv1.setAttribute('title', 'Hello Div');
+
+// Create text node
+var newDivText1 = document.createTextNode('Hello word');
+newDiv1.appendChild(newDivText1);
+console.log(newDiv1);
+newDiv.style.fontSize = '20px';
+list.insertBefore(newDiv1, list.children[0]);
